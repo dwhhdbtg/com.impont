8536a1f69e3d3b8cfffad6d58ad963c1253098ea
yu <1239872125@qq.com>
2018-05-14 13:52:06 +0800

    第三次提交 thymeleaf模板

--- hello/src/main/java/com/impont/controller/FreemarkerController.java
+++ hello/src/main/java/com/impont/controller/FreemarkerController.java
@@ -1,21 +1,31 @@
 package com.impont.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.impont.pojo.Resource;
 
-@RestController
+@Controller
 @RequestMapping("ftl")
 public class FreemarkerController {
 	
 	@Autowired
 	private Resource resource;
-	@RequestMapping("index")
+	/*@RequestMapping("index")
 	public String index(ModelMap map) {
 		map.addAttribute("resource",resource);
 		return "freemarker/index";
-	}
+	}*/
+	@RequestMapping("/index")
+    public String index(ModelMap map) {
+        map.addAttribute("resource", resource);
+        return "freemarker/index";
+    }
+	
+	@RequestMapping("center")
+    public String center() {
+        return "freemarker/center/center";
+    }
 }
